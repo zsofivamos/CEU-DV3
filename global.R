06a8addb76ced03fe5064a9cb366168ab1c6542e
zsofivamos <zsofivamos@gmail.com>
2020-05-31 18:32:28 +0200

    added viz to search function

--- global.R
+++ global.R
@@ -3,11 +3,23 @@
 ## this function takes a word as an input argument and returns the number of times 
 ## that word appears in the data set
 
-search_function <- function(word_input){
-  df %>% filter(word == word_input) %>% 
+search_word <- function(word_input){
+  
+  df <- fread("df.csv") %>% 
+    filter(speaker != "NO SPEAKER") %>% 
+    unnest_tokens(word, script)
+  
+  df %>%
+    filter(word == word_input) %>% 
     group_by(title) %>% 
-    count()
+    count() %>% 
+    arrange(-n) %>% 
+    ggplot(aes(reorder(title,n), n, fill = title)) +
+    geom_col(show.legend = FALSE) +
+    coord_flip() +
+    labs(title = paste0("Total appearances of the word '", word_input,"'"), x="", y = "")+
+    theme_bw()
 
 }
 
-search_function("dr")
+search_word("fly")
